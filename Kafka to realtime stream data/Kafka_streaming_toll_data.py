768ba1c49eb136d3beae62985ee5787816ed999b
Arti Nalegaonkar <56182000+analegaonkar@users.noreply.github.com>
2023-11-28 22:27:01 +0530

    added kafka streaming data project

--- Kafka to realtime stream data/Kafka_streaming_toll_data.py
+++ Kafka to realtime stream data/Kafka_streaming_toll_data.py
@@ -1,3 +1,9 @@
+#create database tolldata;
+#use tolldata;
+#create table livetolldata(timestamp datetime,vehicle_id int,vehicle_type char(15),toll_plaza_id smallint);
+#python3 -m pip install kafka-python
+#python3 -m pip install mysql-connector-python==8.0.31
+
 #Download Kafka
 wget https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz
 
@@ -15,26 +21,9 @@ bin/kafka-server-start.sh config/server.properties
 #Create a topic 'toll'
 cd kafka_2.12-2.8.0
 bin/kafka-topics.sh --create --topic toll --bootstrap-server localhost:9092
-	
-#Download the Toll Traffic Simulator
-wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/toll_traffic_generator.py
-	
-#Configure the Toll Traffic Simulator 
-#Open the toll_traffic_generator.py   
-Set TOPIC = 'set your topic here'
-	
+
 #Run the Toll Traffic Simulator
 python3 toll_traffic_generator.py
-	
-#Configure streaming_data_reader.py
-wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/streaming_data_reader.py
-	
-#Open streaming_data_reader.py and modify the following details so that the program can connect to  mysql server.
-
-TOPIC = 
-DATABASE = 
-USERNAME = 
-PASSWORD = 
 
 #Run streaming_data_reader.py
 python3 streaming_data_reader.py
---
+++ Kafka to realtime stream data/readme.txt
@@ -0,0 +1,2 @@
+1. Continously stream vehile data using toll_trafic_generator.py
+2. read and insert the data into database using streaming_data_reader.py
